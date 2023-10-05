4d82631e5491a6b4449eea3774971ea2c269a94b
vktorrykaliuk <viktorrykalyuk98@gmail.com>
2023-10-05 17:44:19 +0300

    added interceptor

--- app/src/main/java/com/example/the_movie_db_test_task/utils/Constants.kt
+++ app/src/main/java/com/example/the_movie_db_test_task/utils/Constants.kt
@@ -11,7 +11,7 @@ package com.example.the_movie_db_test_task.utils
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val API_TOKEN = "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOWNmYWJiZTUyN2E2MTExYWIyZjVjMWQ1ZmE4ZjNmMyIsInN1YiI6IjY1MWJjNTg5YzUwYWQyMDEwYmZmNGMxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gcIUonMVa7Q2r92rgvbo-RyvigSnP_VO4LMQ9KJvTPM"
+    const val API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOWNmYWJiZTUyN2E2MTExYWIyZjVjMWQ1ZmE4ZjNmMyIsInN1YiI6IjY1MWJjNTg5YzUwYWQyMDEwYmZmNGMxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gcIUonMVa7Q2r92rgvbo-RyvigSnP_VO4LMQ9KJvTPM"
     const val error = "ERROR"
     const val accountId = 20523672
     const val like = "Like"
